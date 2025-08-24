877e2ac3d5cce58b4ca2cc189bb9375c958a8b9f
Austin Merrill <austinm9506@gmail.com>
2025-08-24 02:20:53 -0400

    Added a quick /healthz endpoint for testing routing.

--- cmd/gonk/main.go
+++ cmd/gonk/main.go
@@ -1,3 +1,33 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/AJMerr/gonk/pkg/router"
+)
+
 var Version string
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type:", "application/json, charset-utf8")
+
+	status := struct {
+		OK bool `json:"ok"`
+	}{
+		OK: true,
+	}
+
+	err := json.NewEncoder(w).Encode(status)
+	if err != nil {
+		http.Error(w, "{ok: false}", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	r := router.NewRouter()
+
+	r.Handle("GET /healthz", http.HandlerFunc(healthzHandler))
+
+	http.ListenAndServe(":8080", r)
+}
